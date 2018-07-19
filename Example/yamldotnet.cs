24701f6d3885a0ccef80c3f16aefe9a15448e7f3
xin <xin.liu@geonx.com>
2018-07-19 16:31:55 +0200

    to start

---
+++ Example/yamldotnet.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Dynamic;
+//using System.Globalization;
+using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+using YamlDotNet.Serialization;
+
+namespace yamldotnet
+{
+	class test
+	{
+		static void Main()
+		{
+			var bGraph = new
+			{
+				skin = "skin",
+				material = "material"
+			};
+			
+			var sb = new SerializerBuilder();
+			//sb.DisableAliases();
+			using (var tw = File.CreateText("input.yml"))
+			{
+                sb.Build().Serialize(tw, bGraph);
+            };
+                      
+			Console.WriteLine("work");
+		}		
+	}
+}
